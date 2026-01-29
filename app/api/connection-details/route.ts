e4e3cfcab82055cb3f287d870f3259ff9bdbf495
deepmindru-afk <deepmindru@gmail.com>
2026-01-29 05:17:27 +0300

    Update route.ts

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -33,12 +33,12 @@ export async function POST(req: Request) {
     const body = await req.json();
     const agentName: string = body?.room_config?.agents?.[0]?.agent_name;
     
-    const participantCurr=`portal_${Math.floor(Math.random() * 10_000)}`
+    //const participantCurr=`portal_${Math.floor(Math.random() * 10_000)}`
 
     // Generate participant token
-    const participantName = participantCurr //`va_user_${Math.floor(Math.random() * 10_000)}`;
-    const participantIdentity = participantCurr //`va_user_${Math.floor(Math.random() * 10_000)}`;
-    const roomName = participantCurr //`va_room_${Math.floor(Math.random() * 10_000)}`;
+    const participantName = `va_user_${Math.floor(Math.random() * 10_000)}`;
+    const participantIdentity = `va_user_${Math.floor(Math.random() * 10_000)}`;
+    const roomName = `va_room_${Math.floor(Math.random() * 10_000)}`;
 
     const participantToken = await createParticipantToken(
       { identity: participantIdentity, name: participantName },
