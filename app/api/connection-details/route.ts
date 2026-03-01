2febe1d5cc76f893641afd0f96967d8ec37678da
smatry <smartouses@gmail.com>
2026-03-01 19:03:29 +0300

    cmt

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -17,6 +17,7 @@ const LIVEKIT_URL = process.env.LIVEKIT_URL;
 // don't cache the results
 export const revalidate = 0;
 
+
 export async function POST(req: Request) {
   try {
     if (LIVEKIT_URL === undefined) {
