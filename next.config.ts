06d0ff744c5075cae5d1d921ef4c7280e320b690
deepmindru-afk <deepmindru@gmail.com>
2026-03-03 23:45:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,17 +12,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    formats: ['image/webp'],
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-    // Important: return the modified config
-    config.module.rules.push({
-      test: /\.mjs$/,
-      enforce: 'pre',
-      use: ['source-map-loader'],
-    });
-
-    return config;
+    formats: ['image/webp', 'image/jpeg'],
   },
   headers: async () => {
     return [
