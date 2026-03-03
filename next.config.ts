fee4073da63ff89e01ccc1ea47b0127e69c423ba
deepmindru-afk <deepmindru@gmail.com>
2026-03-03 21:24:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+//import type { NextConfig } from 'next'
+
+const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
   eslint: {
@@ -12,6 +14,37 @@ const nextConfig: NextConfig = {
     // Dangerously allow production builds to successfully complete even if your project has type errors
     ignoreBuildErrors: true,
   },
+  images: {
+    formats: ['image/webp'],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.mjs$/,
+      enforce: 'pre',
+      use: ['source-map-loader'],
+    });
+
+    return config;
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless',
+          },
+        ],
+      },
+    ];
+  },
 };
 
+//module.exports = nextConfig;
 export default nextConfig;
