a1ef9e37781ac3a4bef1cb6e98d5d4b3a1aab095
smatry <smartouses@gmail.com>
2026-03-05 09:26:53 +0300

    cmt

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,10 @@ const nextConfig: NextConfig = {
     }
   },
   productionBrowserSourceMaps: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  //eslint: {
+  //  // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
+  //  ignoreDuringBuilds: true,
+  //},
   typescript: {
     // Dangerously allow production builds to successfully complete even if your project has type errors
     ignoreBuildErrors: true,
