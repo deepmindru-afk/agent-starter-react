1829d2a7f7dd4bd05bc4f019dfd085efad4f152f
deepmindru-afk <deepmindru@gmail.com>
2026-04-05 19:56:28 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   crossOrigin: 'anonymous',
-  allowedDevOrigins: ['*'],
+  allowedDevOrigins: ["*.*","*"],
   experimental: {
     //allowDevelopmentBuild: true,
     serverActions: {
