b78750ec9af33a916cf68846c279255d11a61bc7
deepmindru-afk <deepmindru@gmail.com>
2026-05-17 01:29:11 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  crossOrigin: 'anonymous',
+  allowedDevOrigins: ['*'],
+  experimental: {
+    //allowDevelopmentBuild: true,
+    serverActions: {
+      allowedOrigins: ["*.*","*"]
+    }
+  },
+  productionBrowserSourceMaps: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if your project has type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
